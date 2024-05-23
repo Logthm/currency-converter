204638b2c10a6fb6fdb6fccf3021249a5eb7eaac
Logthm <logthm@outlook.com>
2024-05-23 22:41:10 +0800

    fix detail

--- src/commands/advanced/detail.ts
+++ src/commands/advanced/detail.ts
@@ -12,8 +12,7 @@ export function detail(ctx: Context, config: Config) {
       const code = praser.parse(message, '.')[1]
       if (!code) return session.text('.empty')
 
-      const targetAmount = await converter.convert(1, session.user.base_currency || config.basic.baseCurrency, code)
-      const currentSeparator = session.user.decimal_separator ? session.user.decimal_separator : config.basic.decimalSeparator
+      const targetAmount = await converter.convert(1, code, session.user.base_currency || config.basic.baseCurrency)
       const res = await ctx.database.get('currency', {code: code})
       return session.text('.detail', {
         code: res[0].code,
